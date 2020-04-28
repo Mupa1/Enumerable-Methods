7443b1c4a898ae368152c43e5f59f6ea67af154c
Mupa1 <mupasmail@gmail.com>
2020-04-28 16:41:49 +0200

    add my_each_with_index method

--- enumerable.rb
+++ enumerable.rb
@@ -9,4 +9,15 @@ module Enumerable
     end
     self
   end
+
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    i = 0
+    my_each do |item|
+      yield(item, i)
+      i += 1
+    end
+    self
+  end
 end
--- my_test.rb
+++ my_test.rb
@@ -1,5 +1,7 @@
 load 'enumerable.rb'
 
-[1, 2, 3].my_each do |item|
-  puts item * 2
-end
+puts '****my_each****'
+[1, 2, 3].my_each { |item| puts item * 2 }
+
+puts '****my_each_with_index****'
+[1, 2, 3].my_each_with_index { |item, index| puts "#{item} : #{index}" }
